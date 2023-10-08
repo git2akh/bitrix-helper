4fb7c1582d1460037bdcebd5ffcbea10f64bec04
Aleksei Khokhlov <send2akh@gmail.com>
2023-10-08 22:48:13 +0400

    add git

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env sh
 
+GIT_SCHEME="https";
+GIT_HOST
+GITHUB_AUTHOR="git2akh";
+GITHUB_REPO="bitrix-helper";
+GITHUB_ORIGIN="https://github.com//${GIT_NAME}.git";
+FETCH_DIR=".core";
+
 echo "starting @send2akh/bitrix-helper Ubuntu Installer by NimbleFridge ...";
 #############################################################################
 #############################################################################
 
-
+[ -d "${FETCH_DIR}" ] || mkdir "${FETCH_DIR}";
+cd "${FETCH_DIR}";
+git clone "${GIT_ORIGIN}";
 
 
 #############################################################################
