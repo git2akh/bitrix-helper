ff44ca64cdc6ff9d1bfe1f0500bd025a84efceb0
Aleksei Khokhlov <send2akh@gmail.com>
2023-10-09 11:57:59 +0400

    try latest as clone branch

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -5,7 +5,7 @@ GIT_HOST=github.com
 GIT_AUTHOR="git2akh";
 GIT_REPO="bitrix-helper";
 GIT_ORIGIN="${GIT_SCHEME}://${GIT_HOST}/${GIT_AUTHOR}/${GIT_REPO}.git";
-GIT_ORIGIN="$(curl https://api.github.com/repos/git2akh/bitrix-helper/releases/latest | sed -nE 's;^[[:space:]]{2}"html_url"[[:space:]]*:[[:space:]]*"([^"]+)"[[:space:],]{0,1}$;\1;p;')";
+GIT_BRANCH="$(curl https://api.github.com/repos/git2akh/bitrix-helper/releases/latest | sed -nE 's;^[[:space:]]{2}"tag_name"[[:space:]]*:[[:space:]]*("[^"]+")[[:space:],]{0,1}$;--branch=\1 --single-branch;p;')";
 FETCH_DIR=".core";
 
 echo "starting @send2akh/bitrix-helper Ubuntu Installer by NimbleFridge ...";
@@ -15,7 +15,7 @@ echo "starting @send2akh/bitrix-helper Ubuntu Installer by NimbleFridge ...";
 [ -d "${FETCH_DIR}" ] || mkdir "${FETCH_DIR}";
 cd "${FETCH_DIR}";
 echo "${GIT_ORIGIN}";
-git clone "${GIT_ORIGIN}";
+git clone "${GIT_ORIGIN}" ${GIT_BRANCH};
 cd "${GIT_REPO}";
 pwd;
 
