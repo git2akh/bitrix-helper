3d026012d52be197b0e19ab8341f9efa41eaf550
Aleksei Khokhlov <send2akh@gmail.com>
2023-10-09 12:20:05 +0400

    try 6

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -5,7 +5,7 @@ GIT_HOST=github.com
 GIT_AUTHOR="git2akh";
 GIT_REPO="bitrix-helper";
 GIT_ORIGIN="${GIT_SCHEME}://${GIT_HOST}/${GIT_AUTHOR}/${GIT_REPO}.git";
-GIT_BRANCH="$(curl https://api.github.com/repos/git2akh/bitrix-helper/releases/latest | sed -nE 's;^[[:space:]]{2}"tag_name"[[:space:]]*:[[:space:]]*("[^"]+")[[:space:],]{0,1}$;--branch=\1;p;')";
+GIT_BRANCH="$(curl https://api.github.com/repos/git2akh/bitrix-helper/releases/latest | sed -nE 's;^[[:space:]]{2}"tag_name"[[:space:]]*:[[:space:]]*"([^"]+)"[[:space:],]{0,1}$;--branch=\1;p;')";
 GIT_BRANCH_FLAG_SB="$([ -z "${GIT_BRANCH}" ] || echo "--single-branch")";
 FETCH_DIR=".core";
 
