2b7fd35f629a5158165a465a262c1e193fc079eb
Aleksei Khokhlov <send2akh@gmail.com>
2023-10-09 12:10:02 +0400

    try 3

--- bin/ubuntu.sh
+++ bin/ubuntu.sh
@@ -15,7 +15,7 @@ echo "starting @send2akh/bitrix-helper Ubuntu Installer by NimbleFridge ...";
 [ -d "${FETCH_DIR}" ] || mkdir "${FETCH_DIR}";
 cd "${FETCH_DIR}";
 echo "${GIT_ORIGIN}";
-echo git clone "${GIT_ORIGIN}" ${GIT_BRANCH};
+git clone "${GIT_ORIGIN}" ${GIT_BRANCH};
 cd "${GIT_REPO}";
 pwd;
 
